f9525733d963dcec7338c2f2634a79bfbdbc110e
José Antonio dos Santos Barbosa <jose@josebarbosa.com.br>
2022-05-12 10:20:06 -0300

    Primeiro commit- tela inicial de apresentação de aplicativos

---
+++ teste.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="pt">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Página Inicial de José Barbosa</title>
+  </head>
+  <body>
+    <div class="container-fluid bg-light p-5">
+    	  <h1 class="display-4">Blog de José Barbosa</h1>
+    	  <p class="lead">Site pessoal de José Antonio dos Santos Barbosa</p>
+    	  <hr class="my-4">
+    	  <p>Escolha abaixo um aplicativo de seu interesse:</p>
+	   </div>
+     <div class="container">
+       <div class="row">
+
+         <div class="card col-4">
+           <img src="/imagens/pcs.png" width="100" height="200" class="card-img-top" alt="miniatura da página do simulador de PCS">
+
+           <div class="card-body">
+              <h5 class="card-title">Simulador de Salários do MPU e Judiciário</h5>
+              <p class="card-text">Aplicação que simula a atual remuneração dos servidores do MPU e Judiciário, considerando as Leis <a href="http://www.planalto.gov.br/ccivil_03/_ato2015-2018/2016/lei/L13316.htm">13.316</a> (MPU) e <a href="http://www.planalto.gov.br/ccivil_03/_ato2015-2018/2016/lei/L13317.htm">13.317</a> (PJU), que estruturam as respectivas carreiras.</p>
+              <div class="card-footer">
+                <a href="https://josebarbosa.com.br/novopcs" class="btn btn-secondary">Salários vigentes - 2022</a>
+            </div>
+            </div>
+        </div>
+        <div class="card col-4">
+          <img src="/imagens/pcs5.png" width="100" height="200" class="card-img-top" alt="miniatura da página do simulador de PCS - reajuste de 5% em 2022">
+
+          <div class="card-body">
+             <h5 class="card-title">Cálculo de Salários do MPU e Judiciário - Reajuste de 5%</h5>
+             <p class="card-text">Aplicação que simula a atual remuneração dos servidores do MPU e Judiciário, considerando as alternativas de revisão geral ou modificação da GAJ/GAMPU.</p>
+             <div class="card-footer">
+               <a href="https://josebarbosa.com.br/novopcs5" class="btn btn-secondary">Acessar Simulador Reajuste</a>
+            </div>
+           </div>
+       </div>
+       <div class="card col-4">
+         <img src="/imagens/calculadoraPrevidenciaria.png" width="100" height="200" class="card-img-top" alt="miniatura da página da calculadora de contribuição previdenciária">
+
+         <div class="card-body">
+            <h5 class="card-title">Calculadora de Contribuição Previdenciária</h5>
+            <p class="card-text">Aplicação que  calcula o valor da contribuição previdenciária e do imposto de renda retido na fonte (IRRF), vigentes a partir do ano de 2019 (CLT ou serviço público federal - ativos e inativos).</p>
+            <div class="card-footer">
+              <a href="https://josebarbosa.com.br/previdencia" class="btn btn-secondary"> Calculadora Previdenciária</a>
+            </div>
+          </div>
+      </div>
+    </div>
+  </div>
+
+
+  </body>
+</html>

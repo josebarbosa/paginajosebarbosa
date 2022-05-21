72be248eed2069fc5bd7c9d5cf64ff6f7c0768fa
José Antonio dos Santos Barbosa <jose@josebarbosa.com.br>
2022-05-21 18:50:47 -0300

    Modificação no menu para incluir futuras implementações

--- menu.php
+++ menu.php
@@ -29,7 +29,10 @@
     <li class="nav-item dropdown">
       <a class="nav-link dropdown-toggle" href="#" id="prevDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">Calculadora Previdenciária</a>
       <ul class="dropdown-menu" aria-labelledby="prevDropdown">
-        <li><a class="dropdown-item"  href="https://josebarbosa.com.br/previdencia">Calcula Valor Contribuição (a partir de 2019)</a><li>
+        <li><a class="dropdown-item"  href="https://josebarbosa.com.br/previdencia">Calcular Valor Contribuição (a partir de 2019)</a><li>
+        <li><a class="dropdown-item disabled" aria-disabled="true" href="#">Calcular Benefício</a></li>
+        <li><hr class="dropdown-divider"></li>
+        <li><a class="dropdown-item disabled" aria-disabled="true" href="#">Tempo para a Aposentadoria (RPPS)</a></li>
       </ul>
     </li>
 
